05311833af6da894970feb6ce0ddae38a53c0642
NarenderDhariwal10 <146209993+NarenderDhariwal10@users.noreply.github.com>
2026-01-28 17:04:25 +0530

    express setup and basic utility   custom error, response, async handler

--- src/index.js
+++ src/index.js
@@ -2,5 +2,12 @@ import dotenv from "dotenv";
 dotenv.config();
 import connectDB from "./db/connect_db.js";
 
+const PORT = process.env.PORT || 800
 
-connectDB();
+connectDB()
+.then( () =>{
+    app.listen(PORT , () => console.log(`Server runing on port : ${PORT}`))
+})
+.catch((err) => 
+    console.log(`MONGODB connection error`,err)
+)
