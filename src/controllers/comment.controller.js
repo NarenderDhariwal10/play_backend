df89ee73e9a462a275575e86a6d0fb01751aec8e
NarenderDhariwal10 <146209993+NarenderDhariwal10@users.noreply.github.com>
2026-03-05 22:31:01 +0530

    added delete comment functionality

--- src/controllers/comment.controller.js
+++ src/controllers/comment.controller.js
@@ -75,12 +75,32 @@ const updateComment = asyncHandler(async (req, res) => {
 })
 
 const deleteComment = asyncHandler(async (req, res) => {
-    // TODO: delete a comment
+    
+    const {commentId} = req.params;
+
+    if(!isValidObjectId(commentId)){
+        throw new ApiError(400, "Invalid comment ID")
+    }
+
+    const comment = await Comment.findOneAndDelete({
+        _id: commentId,
+        owner: req.user?._id  
+    });
+    
+    if (!comment) {
+        throw new ApiError(404, "Comment not found or unauthorized");
+    }
+    
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(200,{},"Comment deleted successfully")
+    )
 })
 
 export {
     getVideoComments, 
     addComment, 
     updateComment,
-     deleteComment
-    }
+    deleteComment
+}
