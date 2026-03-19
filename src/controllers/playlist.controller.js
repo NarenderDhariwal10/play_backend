3eb9ef59439e51c0ef28d044f8fc2f0f21314112
NarenderDhariwal10 <146209993+NarenderDhariwal10@users.noreply.github.com>
2026-03-19 21:19:58 +0530

    added playlist controller and routes

---
+++ src/controllers/playlist.controller.js
@@ -0,0 +1,83 @@
+import mongoose, {isValidObjectId} from "mongoose"
+import {Playlist} from "../models/playlist.model.js"
+import {ApiError} from "../utils/ApiError.js"
+import {ApiResponse} from "../utils/ApiResponse.js"
+import {asyncHandler} from "../utils/asyncHandler.js"
+
+
+const createPlaylist = asyncHandler(async (req, res) => {
+    const {name, description} = req.body
+
+    if(!name || ! description){
+        throw new ApiError(400,"Name and Description are required for playlist")
+    }
+
+    const playlist = await Playlist.create({
+        name: name,
+        description: description,
+        owner: req.user?._id
+    })
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(200,playlist,"Playlist created successfully")
+    )
+})
+
+const getUserPlaylists = asyncHandler(async (req, res) => {
+    const {userId} = req.params
+    
+    if(!isValidObjectId(userId)){
+        throw new ApiError(400,"Invalid userId")
+    }
+    
+    
+})
+
+const getPlaylistById = asyncHandler(async (req, res) => {
+    const {playlistId} = req.params
+    
+    if(!isValidObjectId(playlistId)){
+        throw new ApiError(400,"Invalid playlist Id")
+    }
+
+    const playlist = await Playlist.findById(playlistId);
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(200,playlist,"Playlist fetched")
+    )
+})
+
+const addVideoToPlaylist = asyncHandler(async (req, res) => {
+    const {playlistId, videoId} = req.params
+})
+
+const removeVideoFromPlaylist = asyncHandler(async (req, res) => {
+    const {playlistId, videoId} = req.params
+    // TODO: remove video from playlist
+
+})
+
+const deletePlaylist = asyncHandler(async (req, res) => {
+    const {playlistId} = req.params
+    // TODO: delete playlist
+})
+
+const updatePlaylist = asyncHandler(async (req, res) => {
+    const {playlistId} = req.params
+    const {name, description} = req.body
+    //TODO: update playlist
+})
+
+export {
+    createPlaylist,
+    getUserPlaylists,
+    getPlaylistById,
+    addVideoToPlaylist,
+    removeVideoFromPlaylist,
+    deletePlaylist,
+    updatePlaylist
+}
